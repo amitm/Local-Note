ddaa89f4964206bacbbb894328ddbb582c8b25fe
Amit Matani <amatani@ngmoco.com>
2010-11-11 17:15:38 -0800

    saving the result from the activity

--- src/net/digitalmasonry/localnote/CreateNoteActivity.java
+++ src/net/digitalmasonry/localnote/CreateNoteActivity.java
@@ -1,11 +1,21 @@
 package net.digitalmasonry.localnote;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateNoteActivity extends Activity {
+	@Override
 	protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_note);
+    }
+	
+    public void saveNote(View view) {
+    	Intent resultIntent = new Intent();
+    	resultIntent.putExtra(Intent.EXTRA_TEXT, "test");
+    	setResult(Activity.RESULT_OK, resultIntent);
+    	finish();
     }
 }
--- src/net/digitalmasonry/localnote/LocalNote.java
+++ src/net/digitalmasonry/localnote/LocalNote.java
@@ -1,5 +1,7 @@
 package net.digitalmasonry.localnote;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,16 +15,31 @@ public class LocalNote extends ListActivity {
 						     "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.",
 						     "BlackBerry",
 						     "here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc"};
+	private ArrayAdapter<String> adapter;
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.note_list_item, R.id.text1, lv_arr));
+        
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("hello");
+        adapter = new ArrayAdapter<String>(this, R.layout.note_list_item, R.id.text1, list); 
+        setListAdapter(adapter);
     }
     
     public void newNote(View view) {
     	Intent intent = new Intent(this, net.digitalmasonry.localnote.CreateNoteActivity.class);
     	startActivityForResult(intent, CREATE_NOTE_DATA);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if (requestCode == CREATE_NOTE_DATA) {
+    		if (resultCode == RESULT_OK) {
+    			CharSequence sequence = data.getExtras().getCharSequence(Intent.EXTRA_TEXT);
+    			adapter.insert(sequence.toString(), 0);
+    		}
+    	}
     }
 }
